20d0d1dcbab8b54fc12aead7cfe5b77bc31cb44b
trankhanhlam <45313789+trankhanhlam@users.noreply.github.com>
2023-10-03 14:59:35 +0700

    yolo

--- MyLibraryTest/Package.swift
+++ MyLibraryTest/Package.swift
@@ -13,8 +13,7 @@ let package = Package(
             targets: ["MyLibraryTest"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/trankhanhlam/TestSPM", from: "1.0.0"),
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
